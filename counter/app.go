10fe893d92a4a68bfb5d162598fe311b232c2433
Just1689 <Just1689@gmail.com>
2020-01-29 21:05:44 +0200

    log address

--- counter/app.go
+++ counter/app.go
@@ -11,6 +11,7 @@ var ops uint64
 func StartCounter(addr string) {
 	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
 		atomic.AddUint64(&ops, 1)
+		w.Write([]byte("ok"))
 	})
 	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
 		result := atomic.LoadUint64(&ops)
