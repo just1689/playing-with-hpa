c971360370e238f6fcca20ab94f8918b9f4d988d
Just1689 <Just1689@gmail.com>
2020-01-29 19:46:10 +0200

    worker implementation include "DoSomeWork()" that create artificial load of > 100 ms

---
+++ interest/calc.go
@@ -0,0 +1,20 @@
+package interest
+
+import (
+	"github.com/sirupsen/logrus"
+	"math"
+	"time"
+)
+
+func DoSomeWork() {
+	start := time.Now()
+	var total float64 = 0
+	for i := 100000000; i > 0; i-- {
+		x := math.Sqrt(float64(i))
+		y := x
+		x = y
+		total += x
+	}
+	logrus.Println(time.Since(start))
+	logrus.Println(total)
+}
---
+++ interest/worker.go
@@ -0,0 +1,31 @@
+package interest
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/just1689/playing-with-hpa/model"
+	"github.com/nsqio/go-nsq"
+	"github.com/sirupsen/logrus"
+)
+
+func StartInterestWorker(nsqAddr *string) {
+	go func() {
+		config := nsq.NewConfig()
+		q, _ := nsq.NewConsumer(model.BatchTopicName, fmt.Sprint(uuid.New().String(), "#ephemeral"), config)
+		q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+			instruction := model.BatchInstruction{}
+			err := json.Unmarshal(message.Body, &instruction)
+			if err != nil {
+				logrus.Errorln(err)
+			}
+			DoSomeWork()
+			return nil
+		}))
+		if err := q.ConnectToNSQD(*nsqAddr); err != nil {
+			logrus.Panic("Could not connect to NSQ for subscribe", nsqAddr)
+		}
+
+		select {}
+	}()
+}
