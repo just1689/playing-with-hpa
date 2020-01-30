647cd325d146792d8d5035a2bf462183fc94c57a
Captain Justin <just1689@gmail.com>
2020-01-30 09:40:46 +0200

    smaller timeout

--- interest/notify.go
+++ interest/notify.go
@@ -8,7 +8,7 @@ import (
 )
 
 func NotifyDone() {
-	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 75*time.Millisecond)
 	defer cancel()
 	req, err := http.NewRequest("GET", "http://counter.default.svc.cluster.local:8080/add", nil)
 	if err != nil {
