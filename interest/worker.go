8e6b211db2646faec5d7e46eec487bce5ee3690c
Captain Justin <just1689@gmail.com>
2020-01-30 10:48:05 +0200

    limit worker concurrency

--- interest/worker.go
+++ interest/worker.go
@@ -12,22 +12,28 @@ import (
 func StartInterestWorker(nsqAddr string) {
 	go func() {
 		config := nsq.NewConfig()
+		config.MaxInFlight = 10
 		q, _ := nsq.NewConsumer(model.BatchTopicName, fmt.Sprint(uuid.New().String(), "#ephemeral"), config)
-		q.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
-			instruction := model.BatchInstruction{}
-			err := json.Unmarshal(message.Body, &instruction)
-			if err != nil {
-				logrus.Errorln(err)
-			}
-			logrus.Println("Handling instruction: batch #", instruction.BatchID, " on account: ", instruction.AccountID)
-			DoSomeWork()
-			NotifyDone()
-			return nil
-		}))
+		q.AddConcurrentHandlers(HandleMessage, 4)
 		if err := q.ConnectToNSQD(nsqAddr); err != nil {
 			logrus.Panic("Could not connect to NSQ for subscribe", nsqAddr)
 		}
-
 	}()
 	select {}
+}
+
+type MessageHandler struct {
+	MaxInFlight int
+}
+
+var HandleMessage nsq.HandlerFunc = func(message *nsq.Message) error {
+	instruction := model.BatchInstruction{}
+	err := json.Unmarshal(message.Body, &instruction)
+	if err != nil {
+		logrus.Errorln(err)
+	}
+	logrus.Println("Handling instruction: batch #", instruction.BatchID, " on account: ", instruction.AccountID)
+	DoSomeWork()
+	NotifyDone()
+	return nil
 }
